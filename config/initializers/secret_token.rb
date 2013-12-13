0088931e8d8f0c04a518ab614c9d7612c164d048
jojo89 <burtoftallbiz@gmail.com>
2013-12-13 13:23:42 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angel2::Application.config.secret_key_base = '96e687153f12c7f33a622d1046c0d8511d590d4934571b15c60669b0aa0f5f2bf2ce7ef46262fb54568ae8ae39a792a954361358b186ec5037d0080ff107379a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Angel2::Application.config.session_store :cookie_store, key: '_angel2_session'
